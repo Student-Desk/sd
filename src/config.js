c361347404b95e18ea5179cac6cc4116091f2514
Jaydip Patel <jaydippatel@Jaydips-MacBook-Air.local>
2022-05-21 19:08:54 +0530

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const socialAddress = "0xc268FC7770Ad12154FA95C8692CD0b639f33f033";
+export const tokenAddres = "0x36D042F3FAAf266C39321543C090AC0F5D6C03FC";
+export const mintNftAddres = "0x9014019fa9b932E8158b3A8E92f84C24d65dB840";
+export const memoryGameNFTToken = "0x35E5192a1801f291DaDE29c2783D613F40bA9e3f";
+export const contestAddress = "0x8E9Bf941aCF181c6397d33fc8B62B924d3D24d8A";
+export const Luckylottery = "0x4Aa21b70c0Ccb5Cb9D90D71d0Cb05EA0B45Fd9FB";
+export const RandomNumberGeneratorContract = "0x5c9ad489c9D4EbC353E169CA1481Bd629d67C40c";
